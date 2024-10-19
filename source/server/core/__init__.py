9c61cc06971c4ac098b23ca1a1a2daf4d47fa403
TAFH-debug <aushahman2007@gmail.com>
2024-10-19 20:43:39 +0500

    Crucial changes

--- source/server/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .city import *
-from .tile import *
-from .random_map import *
-from .random_names import *
--- source/shared/__init__.py
+++
@@ -1,6 +0,0 @@
-from .net import *
-from .unit_types import *
-from .unit_types import *
-from .unit import *
-from .vmath import *
-from .tree import *
